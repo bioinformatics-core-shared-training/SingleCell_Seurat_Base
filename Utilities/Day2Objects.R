82df540379573dde19b1fa40e7c68913a24beeba
AbbiEdwards <abigail.edwards@cruk.cam.ac.uk>
2026-02-04 00:14:00 +0000

    Norm (unfinished) stuff

---
+++ Utilities/Day2Objects.R
@@ -0,0 +1,18 @@
+library(Seurat)
+library(sctransform)
+library(glmGamPoi)
+library(ggplot2)
+library(tidyverse)
+set.seed = 123 
+
+
+seurat_object_500 <- readRDS("../RObjects/Filtered.500.rds")
+seurat_object_full <- readRDS("../RObjects/Filtered.full.rds")
+
+seurat_object_500 <- SCTransform(seurat_object_500, 
+                                 vars.to.regress = "percent.mt", verbose = FALSE)
+seurat_object_full <- SCTransform(seurat_object_full,
+                                 vars.to.regress = "percent.mt", verbose = FALSE)
+
+saveRDS(seurat_object_500, file = "../RObjects/SCT.500.rds")
+saveRDS(seurat_object_full, file = "../RObjects/SCT.full.rds")
